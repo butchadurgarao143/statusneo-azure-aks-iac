3fe23329b320eee7a410944b442cf1f40755a058
durga <bdpreddy90@gmail.com>
2025-06-20 01:33:02 +0530

    updated

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -1,7 +1,7 @@
 module "aks" {
   source              = "../../modules/aks"
   name                = "alpheya-prod-aks"
-  location            = "centralus"
+  location            = "eastus"
   resource_group_name = "alpheya-tfstate-rg"
   dns_prefix          = "alpheya-prod"
   node_count          = 1
